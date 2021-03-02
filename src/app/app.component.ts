1f2a93ddedcdf6b19d50f13fcb259cd016e87fd8
manalabePatrick <78461378+manalabePatrick@users.noreply.github.com>
2021-03-02 15:54:47 +0800

    Update app.component.ts
    routes edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Library', url: '/library', icon: 'book' },
+    { title: 'Followed', url: '/Outbox', icon: 'paper-plane' },
+    { title: 'Book Shelf', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'My Books', url: '/folder/Archived', icon: 'archive' },
+    { title: 'Letters', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Diary', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Leave', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
