52377237c5ab59c8b58f0e7ec565a7534e067d58
Jvcrsm <201810797@gordoncollege.edu.ph>
2021-03-03 20:38:59 -0800

    Update app.component.ts
    oks na

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'Library', url: '/library', icon: 'book' },
-    { title: 'Followed', url: '/Outbox', icon: 'paper-plane' },
-    { title: 'Book Shelf', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Followed', url: '/Outbox', icon: 'heart-circle' },
+    { title: 'Book Shelf', url: '/folder/Favorites', icon: 'layers' },
     { title: 'My Books', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Letters', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Diary', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Leave', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Letters', url: '/folder/Trash', icon: 'document' },
+    { title: 'Diary', url: '/folder/Trash', icon: 'reader' },
+    { title: 'Leave', url: '/folder/Spam', icon: 'log-out' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
