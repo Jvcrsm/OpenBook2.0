7b822a2c990211d1ed556ca8f29728344ec84f2f
manalabePatrick <78461378+manalabePatrick@users.noreply.github.com>
2021-03-06 03:01:00 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,6 +29,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'shelf',
+    loadChildren: () => import('./pages/shelf/shelf.module').then( m => m.ShelfPageModule)
   }
 
 
