8d37a72795c71f4a2c9e038b29427c5df6668eb6
manalabePatrick <78461378+manalabePatrick@users.noreply.github.com>
2021-03-07 09:43:55 +0800

    Create my-book.page.ts

---
+++ src/app/pages/my-book/my-book.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-book',
+  templateUrl: './my-book.page.html',
+  styleUrls: ['./my-book.page.scss'],
+})
+export class MyBookPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
