22a2e4039219910dcde761fa89e3cfa5edf90279
manalabePatrick <78461378+manalabePatrick@users.noreply.github.com>
2021-05-20 12:47:13 +0800

    my books working

--- src/app/pages/my-book/my-book.page.ts
+++ src/app/pages/my-book/my-book.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { ApiService } from 'src/app/services/api.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-my-book',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyBookPage implements OnInit {
 
-  constructor() { }
+  userId: any;
+  books = [{
+    _id: '1',
+    title: 'Default',
+    summary: 'Default',
+    by:'',
+    genre: []
+  }]
+  constructor(private modalController: ModalController, private storage: LocalStorageService, private apiService: ApiService, private router: Router) { 
+    this.userId = this.storage.userId;
+    this.storage.canPost = true;
+  }
 
   ngOnInit() {
+    // console.log(this.userId);
+    this.showUserBooks();
+  }
+
+  public showUserBooks(){
+    let requestObject = {
+      location: `users/get-user-data/${this.userId}`,
+      method: "GET"
+  }
+
+  this.apiService.makeRequest(requestObject).then((data) => {
+    this.books = data.user.books
+  });
+  }
+
+
+  public viewBook(id, title, by){
+    this.storage.bookId = id;
+    this.storage.bookTitle = title;
+    this.storage.by = by;
+    this.router.navigate(['/view-book']);
+  }
+
+  async presentModal() {
+    // const modal = await this.modalController.create({
+    //   component: PostBookPage,
+    //   cssClass: 'my-custom-class'
+    // });
+    
+    // await modal.present();
+
+    // const { data } = await modal.onWillDismiss();
+    // if(this.storage.didPost){
+    //   this.books.push(data);
+    //   this.storage.didPost = false;
+    // }
+    
   }
 
 }
